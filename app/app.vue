f7e84e2954516c19d2ef21329c094cf3966f1f92
Sanguine-Hexcraft <hexcraft.apothecary@gmail.com>
2026-04-15 15:39:37 -0600

    added /pages and fixed app.vue to include <NuxtPage />

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div class="min-h-screen bg-black flex items-center justify-center">
-    <h1 class="text-4xl text-yellow-400 font-bold">Battle Pasta Games</h1>
-  </div>
+  <NuxtPage />
 </template>
