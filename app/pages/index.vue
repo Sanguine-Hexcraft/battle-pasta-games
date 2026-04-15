0ca332ab12d958f36ecab9b478eaedcadaf9e36c
Sanguine-Hexcraft <hexcraft.apothecary@gmail.com>
2026-04-15 15:47:29 -0600

    added h2 element to index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="min-h-screen bg-black flex items-center justify-center">
+  <div class="min-h-screen bg-black flex flex-col items-center justify-center">
     <h1 class="text-4xl text-yellow-400 font-bold">Battle Pasta Games</h1>
+    <h2 class="text-xl text-white font-light">An indie game studio</h2>
   </div>
 </template>
